bac95a17bcd26f7876fa1bcb24734391ca03d5f8
Gal <galsinai33@gmail.com>
2022-06-05 19:54:21 +0300

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+    return (
+      <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className="mb-4 text-2xl font-light">
+            A React app to search Github profiles and see profile details, repositories and more.
+            You can see other projects of mine on my <strong><a className="text-gray-400" href='https://github.com/GalSInai14'>Github</a></strong> page.
+            My name is <strong> Gal Sinai</strong> and you can visit my <strong>
+                <a className="text-gray-400" href="https://www.linkedin.com/in/gal-sinai-823bab220">Linkedin</a> </strong> page.
+            
+        </p>
+        <p className="text-lg text-gray-400">Version <span className="text-white">1.0.0</span></p>
+        
+      </div>
+    )
+  }
+  
+  export default About
+  
